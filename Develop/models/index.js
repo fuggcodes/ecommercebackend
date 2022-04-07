5ce179c30ebf06e4cfe889f66e74fd40bc999169
David Binyatov <davidbphotography101@gmail.com>
2022-04-07 01:54:40 -0400

    Tags belongToMany Products (through ProductTag)

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -22,6 +22,11 @@ Product.belongsToMany(Tag, {
 });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  //as: 'product_tag',
+  foreignKey: "tag_id",
+});
 
 module.exports = {
   Product,
